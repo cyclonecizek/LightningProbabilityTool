6aaf46c28af54127802ea7ce62252457653c2a32
cyclonecizek <153048634+cyclonecizek@users.noreply.github.com>
2024-07-05 11:33:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def main():
   st.header('Sounding Parameters')
   col1, col2 = st.columns(2)
   with col1:
-    Thompson_Index = st.number_input('Thompson Index (KI - LI)', 0.0, 60.0, step = 0.1, format= "%.1f" )
+    Thompson_Index = st.number_input('Thompson Index (KI - LI)', 0.0, 60.0, step = 0.1, format= "%.1f"%)
     RH = st.number_input('700-500mb Average RH', 0, 100, step=1)
   with col2:
     #wind_average = st.slider('1000-700mb Average U-Wind Component', -40.0, 40.0, 0.5)
-    wind_speed = st.number_input('1000-700mb Average Wind Speed', 0.0, 100.0, step= 0.1, format= "%.1f")
+    wind_speed = st.number_input('1000-700mb Average Wind Speed', 0.0, 100.0, step= 0.1, format= "%.1f"%)
     wind_direction = st.number_input('1000-700mb Average Wind Direction', 0, 360, step = 1)
 
   if st.button('Probability of Lightning'):

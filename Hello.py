95e3c2af3ce2863d7a1faa42dbdeff0061620a04
cyclonecizek <153048634+cyclonecizek@users.noreply.github.com>
2024-08-21 17:21:29 -0400

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title = "Cizek Forecast Tools",
+  )
+
+st.write("Cizek Forecast Tools Homepage")
+
+st.sidebar.success("Select a Forecast Tool above")
+
+
+st.markdown(
+  """
+  This is a test of the Cizek Forecast Tools opening page.
+  """
+)
